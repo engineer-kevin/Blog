260544f70470b6204f3086f12c8c4ecca99ca6dd
卫斯理 <411218668@qq.com>
2022-06-12 11:26:00 +0800

    feature: 算法-贪心算法-56. 合并区间

---
+++ arithmetic/10、贪心算法/56. 合并区间.js
@@ -0,0 +1,40 @@
+// 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+
+// 示例 1：
+
+// 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+// 输出：[[1,6],[8,10],[15,18]]
+// 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+// 示例 2：
+
+// 输入：intervals = [[1,4],[4,5]]
+// 输出：[[1,5]]
+// 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode.cn/problems/merge-intervals
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+  intervals.sort((a, b) => a - b);
+  let prev = intervals[0];
+  const len = intervals.length;
+  const ans = [];
+  for (let i = 0; i < len; i++) {
+    const curr = intervals[i];
+    if (curr[0] <= prev[1]) {
+      prev[1] = Math.max(prev[1], curr[1]);
+    } else {
+      ans.push(prev);
+      prev = curr;
+    }
+  }
+  ans.push(prev);
+  return ans;
+};
+
+console.log(merge([[1,3],[2,6],[8,10],[15,18]]));
+console.log(merge([[1,4],[4,5]]));
