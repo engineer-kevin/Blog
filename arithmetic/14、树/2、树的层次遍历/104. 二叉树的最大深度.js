bdb0603d8078906c3ef6cf33607a19fd8c466945
卫斯理 <411218668@qq.com>
2022-07-08 00:55:18 +0800

    feature: 算法-树-104. 二叉树的最大深度

---
+++ arithmetic/14、树/2、树的层次遍历/104. 二叉树的最大深度.js
@@ -0,0 +1,92 @@
+// 给定一个二叉树，找出其最大深度。
+
+// 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+// 说明: 叶子节点是指没有子节点的节点。
+
+// 示例：
+// 给定二叉树 [3,9,20,null,null,15,7]，
+
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// 返回它的最大深度 3 。
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode.cn/problems/maximum-depth-of-binary-tree
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  // dfs 深度优先搜索
+  // if (root === null) return 0;
+  // const left = maxDepth(root.left);
+  // const right = maxDepth(root.right);
+  // return Math.max(left, right) + 1;
+
+  // bfs 广度优先搜素
+  if (root === null) return 0;
+  let depth = 0;
+  const queue = [root];
+  while (queue.length) {
+    let size = queue.length;
+    while (size > 0) {
+      const node = queue.shift();
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+      size--;
+    }
+    depth++;
+  }
+  return depth;
+};
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+// 迭代实现
+const array2binary = (arr) => {
+  if (!arr || !arr.length) {
+    return null;
+  }
+  let index = 0;
+  const queue = [];
+  const len = arr.length;
+  const head = new TreeNode(arr[index]);
+  queue.push(head);
+
+  while (index < len) {
+    index++;
+    const parent = queue.shift();
+    if (arr[index] !== null && arr[index] !== undefined) {
+      const node = new TreeNode(arr[index]);
+      parent.left = node;
+      queue.push(node);
+    }
+
+    index++;
+    if (arr[index] !== null && arr[index] !== undefined) {
+      const node = new TreeNode(arr[index]);
+      parent.right = node;
+      queue.push(node);
+    }
+  }
+  return head;
+};
+
+console.log(maxDepth(array2binary([3, 9, 20, null, null, 15, 7])));
