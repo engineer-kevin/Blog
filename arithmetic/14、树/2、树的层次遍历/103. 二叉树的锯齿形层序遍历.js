a5eedb56bf94bb738b7daa62bdca1d4523337b4c
卫斯理 <411218668@qq.com>
2022-12-09 08:35:27 +0800

    feature: 算法-树-103. 二叉树的锯齿形层序遍历

---
+++ arithmetic/14、树/2、树的层次遍历/103. 二叉树的锯齿形层序遍历.js
@@ -0,0 +1,106 @@
+// 给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+
+//
+
+// 示例 1：
+
+// 输入：root = [3,9,20,null,null,15,7]
+// 输出：[[3],[20,9],[15,7]]
+// 示例 2：
+
+// 输入：root = [1]
+// 输出：[[1]]
+// 示例 3：
+
+// 输入：root = []
+// 输出：[]
+//
+
+// 提示：
+
+// 树中节点数目在范围 [0, 2000] 内
+// -100 <= Node.val <= 100
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode.cn/problems/binary-tree-zigzag-level-order-traversal
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function (root) {
+  if (root === null) return [];
+  const ans = [];
+  const queue = [root];
+  let isLeftToRight = true;
+  while (queue.length) {
+    const size = queue.length;
+    const level = [];
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+      const val = node.val;
+      if (isLeftToRight) {
+        level.push(val);
+      } else {
+        level.unshift(val);
+      }
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+    isLeftToRight = !isLeftToRight;
+    ans.push(level);
+  }
+  return ans;
+};
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+// 迭代实现
+const array2binary = (arr) => {
+  if (!arr || !arr.length) {
+    return null;
+  }
+  let index = 0;
+  const queue = [];
+  const len = arr.length;
+  const head = new TreeNode(arr[index]);
+  queue.push(head);
+
+  while (index < len) {
+    index++;
+    const parent = queue.shift();
+    if (arr[index] !== null && arr[index] !== undefined) {
+      const node = new TreeNode(arr[index]);
+      parent.left = node;
+      queue.push(node);
+    }
+
+    index++;
+    if (arr[index] !== null && arr[index] !== undefined) {
+      const node = new TreeNode(arr[index]);
+      parent.right = node;
+      queue.push(node);
+    }
+  }
+  return head;
+};
+
+console.log(zigzagLevelOrder(array2binary([3, 9, 20, null, null, 15, 7])));
