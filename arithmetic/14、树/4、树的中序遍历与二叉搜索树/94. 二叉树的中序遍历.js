5616a72ed6a7b86bce6dbefa87e5c99747f0645f
卫斯理 <411218668@qq.com>
2022-12-14 08:21:22 +0800

    feature: 算法-树-94. 二叉树的中序遍历

---
+++ arithmetic/14、树/4、树的中序遍历与二叉搜索树/94. 二叉树的中序遍历.js
@@ -0,0 +1,106 @@
+// 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+
+//
+
+// 示例 1：
+
+// 输入：root = [1,null,2,3]
+// 输出：[1,3,2]
+// 示例 2：
+
+// 输入：root = []
+// 输出：[]
+// 示例 3：
+
+// 输入：root = [1]
+// 输出：[1]
+//
+
+// 提示：
+
+// 树中节点数目在范围 [0, 100] 内
+// -100 <= Node.val <= 100
+//
+
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode.cn/problems/binary-tree-inorder-traversal
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  // 递归实现
+  // const res = [];
+  // function inorder(root) {
+  //   if (!root) return;
+  //   inorder(root.left);
+  //   res.push(root.val);
+  //   inorder(root.right);
+  // }
+  // inorder(root);
+  // return res;
+
+  // 迭代实现
+  const res = [];
+  const stack = [];
+  while (root || stack.length) {
+    if (root) {
+      stack.push(root);
+      root = root.left;
+    } else {
+      root = stack.pop();
+      res.push(root.val);
+      root = root.right;
+    }
+  }
+  return res;
+};
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+// 迭代实现
+const array2binary = (arr) => {
+  if (!arr || !arr.length) {
+    return null;
+  }
+  let index = 0;
+  const queue = [];
+  const len = arr.length;
+  const head = new TreeNode(arr[index]);
+  queue.push(head);
+
+  while (index < len) {
+    index++;
+    const parent = queue.shift();
+    if (arr[index] !== null && arr[index] !== undefined) {
+      const node = new TreeNode(arr[index]);
+      parent.left = node;
+      queue.push(node);
+    }
+
+    index++;
+    if (arr[index] !== null && arr[index] !== undefined) {
+      const node = new TreeNode(arr[index]);
+      parent.right = node;
+      queue.push(node);
+    }
+  }
+  return head;
+};
+
+console.log(inorderTraversal(array2binary([1, null, 2, 3])));
